00cc2f498afe22ae9fd806661159c584511f418e
manojmanumr13 <112933006+manojmanumr13@users.noreply.github.com>
2022-11-19 10:24:49 +0530

    Second Commit

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './style1.css'
 function Color()
 {
  
-  return(<h1 className='skcet'>Hi Dharani How is U r Day</h1>)
+  return(<h1 className='skcet'>Hi Dharani Day</h1>)
 }
  
 const root = ReactDOM.createRoot(document.getElementById('root'));
